41d798b02fee51641a491cf6edfd0aff970fbb7a
Azzerty23 <azzerty23@hotmail.fr>
2019-10-17 22:22:24 +0200

    style : blank on Partie

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class Application
     show = Show.new
     until game.victory? == true
       system("clear")
-      puts "### Partie #{game.counter} ###"
+      puts "       ### Partie #{game.counter} ###"
       show.show_board(game.board)
       game.turn
     end
