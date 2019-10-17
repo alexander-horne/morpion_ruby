7b1a5439b4cc4d253e5bee4814b2b21fcf9c2383
Azzerty23 <azzerty23@hotmail.fr>
2019-10-17 17:13:13 +0200

    fix: boardcase.value

--- lib/boardcase.rb
+++ lib/boardcase.rb
@@ -1,10 +1,10 @@
 class BoardCase
   #TO DO : la classe a 2 attr_accessor, sa valeur en string (X, O, ou vide), ainsi que son identifiant de case
-attr_accessor :valeur
+attr_accessor :value
 
-  def initialize(valeur)
+  def initialize(value)
 	    #TO DO : doit régler sa valeur, ainsi que son numéro de case
-	    @valeur = valeur
+	    @value = value
   end
 
 end
--- show.rb
+++ show.rb
@@ -3,7 +3,7 @@ class Show
 	def show_board(board)
 		#TO DO : affiche sur le terminal l'objet de classe Board en entrée. S'active avec un Show.new.show_board(instance_de_Board)
 		board.boardcase_array.each do |boardcase|
-			print boardcase
+			print boardcase.value
 			print " | "
 		end
 	end
