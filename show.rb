92c9541f53d6f77231dc6a7a97a743a78a53f98c
Azzerty23 <azzerty23@hotmail.fr>
2019-10-17 14:11:02 +0200

    fix show_board

--- show.rb
+++ show.rb
@@ -1,21 +1,23 @@
 class Show
 
-  def show_board(board)
-    #TO DO : affiche sur le terminal l'objet de classe Board en entrée. S'active avec un Show.new.show_board(instance_de_Board)
-   
- def show_case
-  	if @valeur == "X"
-  		puts "-----"
-  		puts "| X |"
-  		puts "-----"
-  	elsif @valeur == "O"
-  		puts "-----"
-  		puts "| O |"
-  		puts "-----"
-  	else
-  		puts "-----"
-  		puts "|   |"
-  		puts "-----"
-  	end
-  end
+	def show_board(board)
+		#TO DO : affiche sur le terminal l'objet de classe Board en entrée. S'active avec un Show.new.show_board(instance_de_Board)
+	end
+
+	def show_case
+		if @valeur == "X"
+			puts "-----"
+			puts "| X |"
+			puts "-----"
+		elsif @valeur == "O"
+			puts "-----"
+			puts "| O |"
+			puts "-----"
+		else
+			puts "-----"
+			puts "|   |"
+			puts "-----"
+		end
+	end
+	
 end
