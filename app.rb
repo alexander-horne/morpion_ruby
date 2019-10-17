e1b5120dfc2168ff2a2e08f7b274ae6aabe85402
Azzerty23 <azzerty23@hotmail.fr>
2019-10-17 16:08:10 +0200

    add Show

--- app.rb
+++ app.rb
@@ -6,13 +6,15 @@ require 'board'
 require 'boardcase'
 require 'game'
 require 'player'
-require 'show'
+require_relative 'show'
 
 class Application
   def perform
     # TO DO : méthode qui initialise le jeu puis contient des boucles while pour faire tourner le jeu tant que la partie n'est pas terminée.
     game = Game.new
+    show = Show.new(game.board)
     unless game.victory?
+      show.show_board(game.board)
       game.turn
     end
     game.game_end
@@ -20,3 +22,5 @@ class Application
   end
 
 end
+
+Application.perform
--- show.rb
+++ show.rb
@@ -2,21 +2,10 @@ class Show
 
 	def show_board(board)
 		#TO DO : affiche sur le terminal l'objet de classe Board en entrée. S'active avec un Show.new.show_board(instance_de_Board)
+		board.boardcase_array.each do |boardcase|
+			print boardcase
+			print " | "
+		end
 	end
 
-	puts
-	puts "     |           |     "
-	puts "-----|-----------|-----"
-	puts "     |TIC-TAC-TOE|     "
-	puts "-----|-----------|-----"
-	puts "     |           |     \n\n"
-	puts "Player 1 :"
-	print "> "
-	gets.chomp = name
-	Player.new(name, X)
-	puts "Player 2 :"
-	print "> "
-	gets.chomp = name
-	Player.new(name, O)
-	perform
 end
