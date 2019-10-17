e006da8098a952cb2d1cb15f86d8c90d95e0e197
Azzerty23 <azzerty23@hotmail.fr>
2019-10-17 12:33:59 +0200

    change require in app.rb

--- app.rb
+++ app.rb
@@ -1,12 +1,17 @@
+require 'bundler'
+Bundler.require
+
+$:.unshift File.expand_path('./../lib/', __FILE__)
+require 'board/'
+require 'boardcase/'
+require 'game'
+require 'player/'
+require 'show/'
+
 class Application
   def perform
     # TO DO : méthode qui initialise le jeu puis contient des boucles while pour faire tourner le jeu tant que la partie n'est pas terminée.
-    # 
-    require './../game/'
-    require './../player/'
-    require './../board/'
-    require './../boardcase/'
-    require './../show/'
+    
   end
 
 end
