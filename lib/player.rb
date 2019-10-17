9a1aa27dca3ca9291366733eec94c28eee61115f
mariollet <mariollet@outlook.fr>
2019-10-17 16:29:34 +0200

    valeur to value

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,11 @@
 class Player
   #TO DO : la classe a 2 attr_reader, son nom et sa valeur (X ou O).
-  attr_reader :nom, :valeur
+  attr_reader :name, :value
 
   
   def initialize(nom, valeur)
     #TO DO : doit régler son nom et sa valeur
-    @nom = nom
-    @valeur = valeur
+    @name = nom
+    @value = valeur
   end
 end
