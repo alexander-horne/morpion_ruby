57a868dac5f604d514cc11353188459d283784be
mariollet <mariollet@outlook.fr>
2019-10-17 15:16:31 +0200

    modif

--- lib/game.rb
+++ lib/game.rb
@@ -17,16 +17,13 @@ class Game
   def turn
     #TO DO : méthode faisant appelle aux méthodes des autres classes (notamment à l'instance de Board). Elle affiche le plateau, demande au joueur ce qu'il joue, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie.
     if @status = "on going"
-      until victory? = true
+      until victory? == true
         player = player1
         play_turn(player)
-        victory?
         player = player2
         play_turn(player)
-        victory?
       end
     end
-
   end
 
   def new_round
